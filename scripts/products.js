39804335aa3f87bf360b486a3ee42d74da8f8969
Delight Sheriff <delightsheriff@gmail.com>
2023-12-03 13:42:54 +0100

    added items to the pages

--- scripts/products.js
+++ scripts/products.js
@@ -1,70 +1,66 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const endpoint = "https://techtrove-tsly.onrender.com/api/all-products";
+document.addEventListener("DOMContentLoaded", async function () {
+  let currentPage = 1; // Track the current page
+  const itemsPerPage = 12; // Number of items to display per page
 
-  fetch(endpoint)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.error("Error fetching products:", error);
-    });
+  async function fetchDataAndDisplay() {
+    try {
+      const response = await fetch(
+        "https://techtrove-tsly.onrender.com/api/all-products"
+      );
+      const data = await response.json();
+
+      const dataArray = data.products;
+
+      // Calculate the start and end indices for the current page
+      const startIndex = (currentPage - 1) * itemsPerPage;
+      const endIndex = startIndex + itemsPerPage;
+
+      // Extract items for the current page
+      const currentItems = dataArray.slice(startIndex, endIndex);
+
+      // Create an empty string to store HTML content
+      let htmlContent = "";
+
+      // Populate HTML content for the selected items using forEach
+      currentItems.forEach((item) => {
+        htmlContent += `
+          <div class="single_gadget">
+            ${item.sale ? "<p>SALE</p>" : ""}
+            <div class="gadget_img">
+              <img src="${item.image}" alt="">
+            </div>
+            <div class="gadget_name">
+              <p>${item.name}</p>
+              <div class="prices">
+                <p>$${item.price}</p>
+                ${item.salePrice ? `<p>$${item.salePrice}</p>` : ""}
+              </div>
+            </div>
+          </div>
+        `;
+      });
+
+      // Display the HTML content inside the #sale-item div
+      document.getElementById("sale-item").innerHTML += htmlContent;
+
+      // Check if there are more items to display
+      const hasMoreItems = endIndex < dataArray.length;
+
+      // Display the "Load More" button if there are more items
+      document.getElementById("load-more").style.display = hasMoreItems
+        ? "block"
+        : "none";
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
+  // Load more button click event
+  document.getElementById("load-more").addEventListener("click", function () {
+    currentPage++; // Increment the current page
+    fetchDataAndDisplay(); // Fetch and display items for the new page
+  });
+
+  // Initial fetch and display
+  fetchDataAndDisplay();
 });
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   const endpoint = "https://techtrove-tsly.onrender.com/api/all-products";
-//   const shopBoxesContainer = document.querySelector(".shop-boxes");
-//   const loadMoreButton = document.getElementById("loadMore");
-//   let displayedProducts = 0;
-
-//   // Fetch the initial set of products
-//   fetchProducts();
-
-//   loadMoreButton.addEventListener("click", () => {
-//     // Fetch more products when the "Load More" button is clicked
-//     fetchProducts();
-//   });
-
-//   function fetchProducts() {
-//     fetch(endpoint)
-//       .then((response) => response.json())
-//       .then((data) => {
-//         // Display 12 more products each time the function is called
-//         const productsToDisplay = data.slice(
-//           displayedProducts,
-//           displayedProducts + 12
-//         );
-//         displayedProducts += productsToDisplay.length;
-
-//         displayProducts(productsToDisplay);
-
-//         // Show/hide the "Load More" button based on whether there are more products
-//         loadMoreButton.style.display =
-//           displayedProducts < data.length ? "block" : "none";
-//       })
-//       .catch((error) => {
-//         console.error("Error fetching products:", error);
-//       });
-//   }
-
-//   function displayProducts(products) {
-//     if (products && products.length > 0) {
-//       products.forEach((product) => {
-//         const productElement = document.createElement("div");
-//         productElement.classList.add("product-box"); // Optional: Add a class for styling
-//         productElement.innerHTML = `
-//           <h3>${product.name}</h3>
-//           <p>${product.description}</p>
-//           <p>Price: $${product.price}</p>
-//           <hr>
-//         `;
-//         shopBoxesContainer.appendChild(productElement);
-//       });
-//     } else {
-//       if (displayedProducts === 0) {
-//         // Display a message if there are no products
-//         shopBoxesContainer.innerHTML = "<p>No products available.</p>";
-//       }
-//     }
-//   }
-// });
